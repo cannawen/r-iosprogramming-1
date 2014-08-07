7e1fceec00ddeb24e3035e774563e6e8fcfe4576
Canna Wen <cwen@pivotallabs.com>
2014-08-07 06:27:33 -0400

    Creating a second scene in storyboard and custom class

---
+++ r-ioschallenges-1/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  r-ioschallenges-1
+//
+//  Created by Canna Wen on 2014-08-07.
+//  Copyright (c) 2014 Canna Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@end
