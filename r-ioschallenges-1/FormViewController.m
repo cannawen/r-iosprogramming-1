4d6eafd74f01f71985a4bbd14d61877955cc4339
Canna Wen <cwen@pivotallabs.com>
2014-08-07 06:13:01 -0400

    Initial commit

--- r-ioschallenges-1/FormViewController.m
+++ r-ioschallenges-1/FormViewController.m
@@ -1,13 +1,18 @@
 //
-//  ViewController.h
+//  ViewController.m
 //  r-ioschallenges-1
 //
 //  Created by Canna Wen on 2014-08-07.
 //  Copyright (c) 2014 Canna Wen. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "FormViewController.h"
 
-@interface ViewController : UIViewController
+@interface FormViewController ()
+
+@end
+
+@implementation FormViewController
+
 
 @end
