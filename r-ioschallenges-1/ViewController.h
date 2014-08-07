bf60766d539b0160d1a5e8a66c741c85c9267bb7
Canna Wen <cwen@pivotallabs.com>
2014-08-07 06:11:07 -0400

    Initial Commit

---
+++ r-ioschallenges-1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  r-ioschallenges-1
+//
+//  Created by Canna Wen on 2014-08-07.
+//  Copyright (c) 2014 Canna Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ r-ioschallenges-1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  r-ioschallenges-1
+//
+//  Created by Canna Wen on 2014-08-07.
+//  Copyright (c) 2014 Canna Wen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
